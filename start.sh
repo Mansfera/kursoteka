4c1f3fc093dec3e52d3bce938b646f853f08aa52
Mansfera <mansferagg@gmail.com>
2024-11-20 17:55:59 +0200

    fix

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
-# Commit changes with a default message (you can customize it)
-git commit -m "Auto-commit from start.sh" -a
-
-# Push changes to the remote repository
-git push origin main  # Replace 'main' with your default branch if necessary
-
-# Pull the latest changes from the remote repository
-git pull origin main  # Replace 'main' if necessary
-
+git pull
 node app
