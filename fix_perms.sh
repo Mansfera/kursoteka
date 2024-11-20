ba9b59967da84c8909c59cbd2d43afd428d35195
Mansfera <mansferagg@gmail.com>
2024-11-20 17:49:21 +0200

    fix

--- fix_perms.sh
+++ fix_perms.sh
@@ -2,11 +2,10 @@
 chmod -R u+w .git/objects
 sudo chown -R kursoteka:kursoteka .git
 chmod 755 /home/kursoteka/public_html
-chmod 644 /home/kursoteka/public_html/promocodes.json
-chmod 644 /home/kursoteka/public_html/users.json
 chmod 644 /home/kursoteka/public_html/courses.json
 sudo chown kursoteka:kursoteka /home/kursoteka/public_html/
 chmod +x start.sh
+chmod +x update.sh
 
 # DB fix:
 chmod 666 db/users.db
