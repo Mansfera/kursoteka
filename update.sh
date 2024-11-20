44447a592ce00c7c471e7dff4d585a0ae5fe4e11
Mansfera <mansferagg@gmail.com>
2024-11-20 22:10:39 +0200

    Auto update 39:10:22 20.11.2024

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
-git commit -a -m "server push"
+# Get formatted date (ss:mm:hh dd.mm.yyyy)
+date_formatted=$(date "+%S:%M:%H %d.%m.%Y")
+git commit -a -m "Auto update ${date_formatted}"
 git push
 git pull
