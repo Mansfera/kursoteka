793c8afdb7bbed1a96d3eb4340b7238f645531cd
Mansfera <mansferagg@gmail.com>
2024-11-25 16:02:47 +0200

    t18 40

--- fix_perms.sh
+++ fix_perms.sh
@@ -6,9 +6,3 @@ chmod 644 /home/kursoteka/public_html/courses.json
 sudo chown kursoteka:kursoteka /home/kursoteka/public_html/
 chmod +x start.sh
 chmod +x update.sh
-
-# DB fix:
-chmod 666 db/users.db
-chmod 777 db/
-sudo chown kursoteka:kursoteka db/users.db
-sudo chown kursoteka:kursoteka db/
