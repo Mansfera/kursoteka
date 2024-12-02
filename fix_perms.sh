7affc91fc657929725e1027ea019a2aa992e1ea5
Mansfera <mansferagg@gmail.com>
2024-12-02 22:37:10 +0200

    fix

--- fix_perms.sh
+++ fix_perms.sh
@@ -2,7 +2,9 @@
 chmod -R u+w .git/objects
 sudo chown -R kursoteka:kursoteka .git
 chmod 755 /home/kursoteka/public_html
-chmod 644 /home/kursoteka/public_html/courses.json
+chmod 644 /home/kursoteka/public_html/
 sudo chown kursoteka:kursoteka /home/kursoteka/public_html/
+# Ensure node app has write permissions
+chmod -R 775 /home/kursoteka/public_html/
 chmod +x start.sh
 chmod +x update.sh
