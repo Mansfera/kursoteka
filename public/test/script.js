3066935ec8e36a8151e0fb570bb3a247ef0cfda4
Mansfera <mansferagg@gmail.com>
2024-12-12 18:16:17 +0200

    fix sync

--- public/test/script.js
+++ public/test/script.js
@@ -814,6 +814,7 @@ async function sendTestResult() {
     JSON.stringify(updatedUncompletedTests)
   );
   setCookie(`lastUncompletedTestsUpdate-${course}`, Date.now());
+  syncUncompletedTests();
   let _test_id = test_id;
   let test_abcd_q = [];
   let test_hron_q = [];
